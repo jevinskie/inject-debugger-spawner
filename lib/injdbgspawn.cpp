3948189c87ba4841146db83363ea6cdab20da211
Jevin Sweval <jevinsweval@gmail.com>
2022-07-08 12:26:25 -0700

    do fetchcontent

--- lib/injdbgspawn.cpp
+++ lib/injdbgspawn.cpp
@@ -1,10 +1,12 @@
 #undef NDEBUG
 #include <cassert>
 #include <cstdio>
+#include <string>
 
 #include <fmt/format.h>
 
 #include "frida-gum.h"
+#include "subprocess.hpp"
 
 struct Listener {
     GObject parent;
@@ -18,6 +20,32 @@ struct _InvocationData {
     gpointer path;
 };
 
+static bool should_spawn_debugger() {
+    bool spawn = false;
+    spawn      = true;
+    fmt::print("should_spawn_debugger: {}\n", spawn);
+    return spawn;
+}
+
+static void clear_from_ld_preload() {
+    const auto orig_cstr = getenv("LD_PRELOAD");
+    assert(orig_cstr);
+    std::string pruned;
+    char *op = orig_cstr;
+    const char *colon;
+    while (colon = strchr(op, ':')) {
+        const std::string lib{op, colon - op};
+    }
+}
+
+static void spawn_debugger() {
+    std::string proc_name{"gnome-calculator"};
+    auto proc = subprocess::Popen({proc_name});
+}
+
 __attribute__((constructor)) static void init_injdbgspawn() {
     fmt::print("hello from inj ctor.\n");
+    if (should_spawn_debugger()) {
+        spawn_debugger();
+    }
 }
