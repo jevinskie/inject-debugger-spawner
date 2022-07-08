9df78f9b9ccc4d8fc73f90b367874d46bc9dd9e5
Jevin Sweval <jevinsweval@gmail.com>
2022-07-07 18:04:53 -0700

    babby steps

--- lib/injdbgspawn.cpp
+++ lib/injdbgspawn.cpp
@@ -4,6 +4,18 @@
 
 #include "frida-gum.h"
 
+struct Listener {
+    GObject parent;
+};
+
+enum HookId {
+    HOOK_VFORK,
+};
+
+struct _InvocationData {
+    gpointer path;
+};
+
 __attribute__((constructor)) static void init_injdbgspawn() {
     printf("hello from inj ctor.\n");
 }
