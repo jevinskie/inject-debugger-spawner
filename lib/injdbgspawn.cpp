dfdb16822b53085e0d49e32314d43d4c5a286a70
Jevin Sweval <jevinsweval@gmail.com>
2022-07-08 13:45:54 -0700

    clear_from_ld_preload()

--- lib/injdbgspawn.cpp
+++ lib/injdbgspawn.cpp
@@ -1,7 +1,9 @@
 #undef NDEBUG
 #include <cassert>
 #include <cstdio>
+#include <cstdlib>
 #include <string>
+#include <vector>
 
 #include <fmt/format.h>
 
@@ -30,12 +32,32 @@ static bool should_spawn_debugger() {
 static void clear_from_ld_preload() {
     const auto orig_cstr = getenv("LD_PRELOAD");
     assert(orig_cstr);
-    std::string pruned;
-    char *op = orig_cstr;
-    const char *colon;
-    while (colon = strchr(op, ':')) {
-        const std::string lib{op, colon - op};
+    const auto *orig_cstr_nul_ptr = orig_cstr + strlen(orig_cstr);
+    const char *op                = orig_cstr;
+    const char *colon             = nullptr;
+    const std::string our_lib{"libinjdbgspawn.so"};
+    std::vector<std::string> libs;
+    while ((colon = strchr(op, ':'))) {
+        const std::string lib{op, (size_t)(colon - op)};
+        if (!lib.ends_with(our_lib)) {
+            libs.emplace_back(lib);
+        }
+        op = colon + 1;
     }
+    if (op != orig_cstr_nul_ptr) {
+        const std::string lib{op, (size_t)(orig_cstr_nul_ptr - op)};
+        if (!lib.ends_with(our_lib)) {
+            libs.emplace_back(lib);
+        }
+    }
+    std::string pruned;
+    if (!libs.empty()) {
+        for (size_t i = 0; i < libs.size() - 1; ++i) {
+            pruned += libs[i] + ":";
+        }
+        pruned += libs[libs.size() - 1];
+    }
+    assert(!setenv("LD_PRELOAD", pruned.c_str(), true));
 }
 
 static void spawn_debugger() {
@@ -45,6 +67,7 @@ static void spawn_debugger() {
 
 __attribute__((constructor)) static void init_injdbgspawn() {
     fmt::print("hello from inj ctor.\n");
+    clear_from_ld_preload();
     if (should_spawn_debugger()) {
         spawn_debugger();
     }
